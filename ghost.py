190684ffd8451368cd2e75010f35f8fb52539fbf
Tiago Onofre Araujo <tiagojack84@gmail.com>
2020-03-24 17:23:58 -0300

    incresase on_message event

--- ghost.py
+++ ghost.py
@@ -1,5 +1,8 @@
-# Main module
+# Main module --> discord API
 import discord
+
+# For random choices ( dice, coin, etc )
+import random
 
 ''' It is strongly recommended that the logging module is configured, 
 as no errors or warnings will be output if it is not set up. '''
@@ -21,6 +24,9 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('\033[31m{0.user} is OnLine!\033[0m'.format(client))
+    print(client.user.name)
+    print(client.user.id)
+
 
 
 @client.event
@@ -28,7 +34,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$Teste'):
+    if message.content.lower().startswith('$teste'):
         await message.channel.send('Hello!')
+    
+    if message.content.lower().startswith('$tiago'):
+        await message.channel.send('One of my creators!')
+    
+    if message.content.lower().startswith('$roll_coin'):
+        #if message.author.id == "id":
+        coin = random.randint(1, 2)
+        if coin == 1:
+            await message.add_reaction('🙂')
+        elif coin == 2:
+            await message.add_reaction('👑') 
 
-client.run('NjkyMDEzNDU3MTIxNDc2NzA5.XnojQg.iRpw8wOXqLsknm0hSClnyeL3FFE')
+# ALERT --> THIS TOKEN CANNOT BE SHARE!!!
+client.run('NTI1NTE1NzI1NTcwNTcyMzU4.XnpK4A.omnlE_0Zq6V24sy5to_WF2fEiXo')
