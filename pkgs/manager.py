04019a11405f2a3db49ccf04b2d138363cf21ae4
Tiago Onofre Araujo <tiagojack84@gmail.com>
2020-03-28 16:43:59 -0300

    added manager class and modularized files

---
+++ pkgs/manager.py
@@ -0,0 +1,36 @@
+ 
+
+class Manager(object):
+
+     def __init__( self ): #{{{
+
+         self.msg_id =''
+         self.user_id =''
+
+    #}}}
+
+     def set_msg_id( self, msg_id ): #{{{
+
+        self.msg_id = msg_id
+
+    #}}}
+
+     def set_user_id( self, user_id): #{{{
+
+        self.user_id = user_id
+
+    #}}}
+
+     def get_msg_id( self ): #{{{
+
+        return self.msg_id
+
+    #}}}
+
+     def get_user_id( self ): #{{{
+
+        return self.user_id
+
+    #}}}
+
+
